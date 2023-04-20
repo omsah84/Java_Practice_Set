12b3f030d3634b4f24844c0a1afe340ad52a9efc
omsah <huncyoms@gmail.com>
2023-04-20 10:09:34 -0400

    commit

---
+++ Basic_Program/cP.cpp
@@ -0,0 +1,19 @@
+// #include<stdio.h>
+#include <iostream>
+using namespace std;
+ 
+int main()
+{
+ 
+    // Declare the variables
+    int num;
+ 
+    // Input the integer
+    cout << "Enter the integer: ";
+    cin >> num;
+ 
+    // Display the integer
+    cout << "Entered integer is: " << num;
+ 
+    return 0;
+}
